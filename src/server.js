bbdc02e80665393006e77a5abe9d285df6818f06
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-03-20 16:14:12 -0300

    att para carregar o front

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/produtos', produtoRoutes);
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
