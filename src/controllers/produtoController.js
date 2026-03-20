53e38cbff5dadcceb103d68b89ea24111c402978
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-03-20 15:46:39 -0300

    Indentações e formatações

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,45 +1,45 @@
 import db from '../config/db.js'
 
-export const listarProdutos = (req,res) => {
+export const listarProdutos = (req, res) => {
 
     db.query('SELECT * FROM produtos', (err, results) => {
 
-        if(err) return res.status(500).json ({erro:'Erro ao listar produtos'});
+        if (err) return res.status(500).json({ erro: 'Erro ao listar produtos' });
         res.json(results)
     })
 }
 
-export const inserirProdutos = (req,res) => {
+export const inserirProdutos = (req, res) => {
 
-    const { nome, descricao, preco, estoque, categoria} = req.body;
+    const { nome, descricao, preco, estoque, categoria } = req.body;
     const sql = 'INSERT INTO produtos (nome, descricao, preco, estoque, categoria) VALUES (?, ?, ?, ?, ?)'
     db.query(sql, [nome, descricao, preco, estoque, categoria], err => {
 
-        if(err) return res.status(500).json ({erro:'Erro ao inserir produto'});
-        res.json({mensagem:'Produto inserido com sucesso!'})
+        if (err) return res.status(500).json({ erro: 'Erro ao inserir produto' });
+        res.json({ mensagem: 'Produto inserido com sucesso!' })
     });
 };
 
-export const atualizarProduto = (req,res) => {
+export const atualizarProduto = (req, res) => {
 
     const { id } = req.params;
-    const {  nome, descricao, preco, estoque, categoria} = req.body;
+    const { nome, descricao, preco, estoque, categoria } = req.body;
     const sql = 'UPDATE produtos SET nome=?, descricao=?, preco=?, estoque=?, categoria=? WHERE id=?'
-    db.query(sql,  [nome, descricao, preco, estoque, categoria, id], err => {
+    db.query(sql, [nome, descricao, preco, estoque, categoria, id], err => {
 
-        if(err) return res.status(500).json ({erro:'Erro ao atualizar produto'});
-        res.json({mensagem:'Produto atualizado com sucesso!'})
+        if(err) return res.status(500).json({ erro: 'Erro ao atualizar produto' });
+        res.json({ mensagem: 'Produto atualizado com sucesso!' })
     });
 }
 
-export const excluirProduto = (req,res) => {
+export const excluirProduto = (req, res) => {
 
     const { id } = req.params;
     const sql = 'DELETE FROM produtos WHERE id=?'
 
     db.query(sql, [id], err => {
 
-        if(err) return res.status(500).json ({erro:'Erro ao excluir produto'});
-        res.json({ mensagem:'Produto excluido com sucesso!'});
+        if (err) return res.status(500).json({ erro: 'Erro ao excluir produto' });
+        res.json({ mensagem: 'Produto excluido com sucesso!' });
     })
 }
